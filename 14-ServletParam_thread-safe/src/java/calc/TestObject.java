d72413e96758cd1a506f5c248602fba2a468c906
Nikolay Perov <nikolayp2013@gmail.com>
2019-07-23 15:14:51 +0400

    initial project version

---
+++ 14-ServletParam_thread-safe/src/java/calc/TestObject.java
@@ -0,0 +1,17 @@
+package calc;
+
+public class TestObject {
+    
+    private String name;
+
+    public TestObject(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    
+
+}
