ae3def844b0dc4adbbe1289b550c31d2dab69e54
Nikolay Perov <nikolayp2013@gmail.com>
2019-08-01 11:39:38 +0400

    initial project version

---
+++ JsfLibrary_8/src/java/ru/javabegin/training/web/beans/LocaleChanger.java
@@ -0,0 +1,27 @@
+package ru.javabegin.training.web.beans;
+
+import java.io.Serializable;
+import java.util.Locale;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleChanger implements Serializable {
+
+    private Locale currentLocale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+    public LocaleChanger() {
+    }
+
+    public void changeLocale(String localeCode) {
+        currentLocale = new Locale(localeCode);
+    }
+
+    public Locale getCurrentLocale() {
+        return currentLocale;
+    }
+
+
+}
